b65dd3f5ba07509cfe070f9279738130bd6c142d
OhByungJik1222 <dotory15@gmail.com>
2026-01-22 21:33:12 +0900

    2026-01-23

---
+++ 알고리즘 기초 1/04. 수학 1/04. 1978번 소수 찾기.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> seive_of_eratosthenes(int n) {
+    vector<int> is_prime(n + 1, 1);
+    is_prime[0] = 0;
+    is_prime[1] = 0;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                is_prime[j] = 0;
+            }
+        }
+    }
+    return is_prime;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<int> num(n);
+    int max_val = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> num[i];
+        max_val = max(max_val, num[i]);
+    }
+
+    vector<int> is_prime = seive_of_eratosthenes(max_val);
+
+    int ans = 0;
+    for (int x : num) {
+        if (is_prime[x]) ans++;
+    }
+    cout << ans;
+
+    return 0;
+}
