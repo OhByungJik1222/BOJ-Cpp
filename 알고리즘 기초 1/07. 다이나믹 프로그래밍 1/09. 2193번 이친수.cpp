d286fc2f5327ec0fa4dde8f9d958994fed4b36e5
OhByungJik1222 <dotory15@gmail.com>
2026-02-16 14:05:42 +0900

    2026-02-16

---
+++ 알고리즘 기초 1/07. 다이나믹 프로그래밍 1/09. 2193번 이친수.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<vector<long long>> dp(n + 1, vector<long long> (2, 1));
+    dp[1][0] = 0;
+    for (int i = 2; i <= n; i++) {
+        dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+        dp[i][1] = dp[i - 1][0];
+    }
+    cout << dp[n][0] + dp[n][1];
+
+    return 0;
+}
